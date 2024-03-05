a8d76cd261c178f41524e47a3ad789f244fc2929
derkder <1026305391@qq.com>
2024-03-05 22:12:01 +0000

    fix: 整理一下DemoScene结构，暴力修了一下莫名其妙的相机下坠问题

---
+++ Assets/Scripts/CameraTransformController.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//暴力控制这个癫子摄像机
+public class CameraTransformController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(transform.position.y);
+        if(transform.position.y < 0.2f)
+        {
+            Debug.Log("Camera Offset Descending" + transform.position.y);
+            Vector3 newPosition = transform.position;
+            newPosition.y += 1;
+            transform.position = newPosition;
+        }
+    }
+}
