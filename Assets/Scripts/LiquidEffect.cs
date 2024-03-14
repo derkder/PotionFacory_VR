05f862601775cc8a060f82bc2e060c8de56475fa
derkder <1026305391@qq.com>
2024-03-14 21:54:37 +0000

    fix: 液体效果修真

--- Assets/Scripts/LiquidEffect.cs
+++ Assets/Scripts/LiquidEffect.cs
@@ -27,7 +27,7 @@ public class LiquidEffect : MonoBehaviour
     private float _minAmont;
     [SerializeField]
     private float _maxAmont;
-    private float _step = 0.0002f;
+    private float _step = 0.001f;
 
 
     void Start()
@@ -44,11 +44,11 @@ public class LiquidEffect : MonoBehaviour
     void Update()
     {
         _material.SetFloat("_WorldCoordY", transform.position.y);
-
+        _material.SetFloat("_FillAmount", _curAmount);
         if (Vector3.Distance(PotionRecharge.position, transform.position) < Radius)
         {
+            Debug.Log("asdasdasd");
             _curAmount = _minAmont;
-            _material.SetFloat("_FillAmount", _curAmount);
         }
     }
 
@@ -58,17 +58,18 @@ public class LiquidEffect : MonoBehaviour
         float dotProduct = Vector3.Dot(forwardDirection, Vector3.up);
 
         // 判断夹角是否超过90°
-        if (dotProduct < Mathf.Cos(40 * Mathf.Deg2Rad))
+        if (dotProduct < Mathf.Cos(50 * Mathf.Deg2Rad))
         {
             //下面的判断只会进入一次
-            if(Mathf.Abs(_minAmont - _curAmount) <= _step - 0.0001)
+            if(Mathf.Abs(_minAmont - _curAmount) <= _step - 0.0005)
             {
                 //第一次开始倾倒，因为旋转，所以需要增加偏移
                 //其实最好是偏移跟着rotation慢慢lerp到这里0.05f
                 //试图修复这里一下子倾倒总会有一下子变满的错误
+                Debug.Log("FirstBiggerThan50");
                 _maxAmont += 0.4f;
-                _minAmont += 0.06f;
-                _curAmount = _minAmont;
+                _minAmont += 0.08f;
+                _curAmount = _minAmont + _step;
                 if(Vector3.Distance(CookPot.position, this.transform.position) < Radius)
                 {
                     HasPoured?.Invoke();
