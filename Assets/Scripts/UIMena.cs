e0773b278d7c76f1d70cbf25211ff0b94d989ce8
w15050jl <1123462319@qq.com>
2024-03-14 15:43:59 +0000

    UI&Sound effect

---
+++ Assets/Scripts/UIMena.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class UIMenu : MonoBehaviour
+{
+    public Transform head;
+    public float spawnDistance = 2;
+    public GameObject menu;
+
+    [SerializeField]
+    private InputActionAsset _actionAsset;
+    private InputAction _selectAction;
+    private Vector3 relativePosition;
+
+    void Start()
+    {
+        _selectAction = _actionAsset.FindActionMap("XRI RightHand").FindAction("AButton");
+        if (_selectAction == null)
+        {
+            Debug.LogError("Action not found.");
+            return;
+        }
+
+        _selectAction.performed += _ => menu.SetActive(!menu.activeSelf);
+        relativePosition = new Vector3(0, -0.5f, spawnDistance); // Adjusted relative position
+    }
+
+    void Update()
+    {
+        if (menu.activeSelf)
+        {
+            // Position the menu at a specific distance in front of the player
+            Vector3 desiredPosition = head.position + head.forward.normalized * spawnDistance;
+            // Adjust the height relative to the head's position to prevent it from appearing too high
+            desiredPosition.y = head.position.y + relativePosition.y;
+
+            menu.transform.position = desiredPosition;
+
+            // Make the menu face the player
+            menu.transform.LookAt(head);
+            // This last line is to flip the menu to face the correct way if it's backwards
+            menu.transform.forward = -menu.transform.forward;
+        }
+    }
+}
