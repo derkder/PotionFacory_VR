44f172d967949b6899e752a1c78596d87931bdaf
Sun <haoyunxiaosun@gmail.com>
2024-03-16 03:09:52 +0800

    update/

---
+++ Assets/Scripts/CheckGravity.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkGravity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+         
+        if(rb != null) {
+            rb.useGravity=true;
+            rb.isKinematic=false;
+        }
+    }
+    // void  Awake() {
+    //     Rigidbody rb = GetComponent<Rigidbody>();
+         
+    //     if(rb != null) {
+    //         rb.useGravity=true;
+    //         rb.isKinematic=false;
+    //     }
+    // }
+}
